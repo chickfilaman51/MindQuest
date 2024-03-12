380e04b5cf780a12aaad3fa3f6bd1333204c659c
asanku091011 <sanku.abhiram@gmail.com>
2024-03-11 21:41:33 -0400

    Finished the questions

--- placement.py
+++ placement.py
@@ -6,9 +6,14 @@ import rules
 questions = {
     1: ["9 - 2 = ?", "13 + 4 = ?"],
     2: ["17 + 93 = ?", "132 - 74 = ?"],
-    3: ["17 * 13 = ?", "621 / 27 = ?"],
+    3: ["17 x 13 = ?", "621 / 27 = ?"],
     4: ["(28/7) / (6/7) = ?", "√49 = ?"],
-    5: ["(28/7) / (6/7) = ?", "√49 = ?"],
+    5: ["6.21 x 4.3 = ?", "8.3 x 32 = ?"],
+    6: ["What is the volume of a rectangluar prism with sides of length 2, 3, and 5?","What is the hypotnuese of a right triangle with legs of length 5 and 12"],
+    7: ["If 2(3x+4)=(4x+5)/3, what is x?","If (x/19)=4x-23, what is x?"],
+    8: ["What is the greatest possible solution of x if 3x^2+4x+1=0","What is the product of the two solutions for x in this equation: 8x^2+45x+25=0"],
+    9: ["The sum of two numbers is 18, and the product of these two numbers is 56. What are the numbers?","Doug went to a conference in a city 120 km away. On the way back, due to road construction, he had to drive 10 km/h slower, which resulted in the return trip taking 2 hours longer.\nHow fast did he drive on the way to the conference?"],
+    10: ["From a point on the ground 47 feet from the foot of a tree, the angle of elevation of the top of the tree is 35º. Find the height of the tree to the nearest foot.","An 8 foot metal guy wire is attached to a broken stop sign to secure its position until repairs can be made.\nAttached to a stake in the ground, the guy wire makes an angle of 51º with the ground. How far from the foot of the stop sign is the stake, to the nearest tenth of a foot?"]
     # Add questions for other grades similarly
 }
 
